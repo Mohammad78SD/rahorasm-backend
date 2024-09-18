d164dc4b7b04b52fbae89cbe819cbb76d7a96da6
Mohammad78SD <mohammadsd2000@gmail.com>
2024-09-18 20:00:32 +0330

    feat: Add django_jalali, blog, and ckeditor apps to INSTALLED_APPS

--- rahorasm/rahorasm/settings.py
+++ rahorasm/rahorasm/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_jalali',
     'UserManager',
+    'blog',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
