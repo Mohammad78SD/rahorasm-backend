a0d66694f653f1f33df81c4c7ac46c08cb9f0be1
Mohammad78SD <mohammadsd2000@gmail.com>
2024-09-23 12:24:12 +0330

    feat: Add Continent model and update related files
    This commit adds a new model called Continent and updates the admin.py, models.py, and serializers.py files accordingly.
    
    Refactor the settings.py file
    
    This commit refactors the CORS settings in the settings.py file.
    
    Refactor the settings.py file
    
    This commit updates the STATIC_ROOT path and adds CORS settings in the settings.py file.
    
    Refactor the settings.py file
    
    This commit updates the STATIC_ROOT path in the settings.py file.
    
    Refactor UserManager URLs and add logout functionality
    
    This commit updates the UserManager URLs and adds logout functionality.

--- rahorasm/TourManager/views.py
+++ rahorasm/TourManager/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from django.db.models import OuterRef, Subquery
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import City, Country, AirLine, Airport, Package, Tour
+from .models import City, Country, AirLine, Airport, Package, Tour, Continent
 from .serializers import (
     CitySerializer,
     CountrySerializer,
@@ -8,6 +12,7 @@ from .serializers import (
     AirportSerializer,
     PackageSerializer,
     TourSerializer,
+    ContinentSerializer
 )
 from .filters import (
     CityFilter,
@@ -17,6 +22,7 @@ from .filters import (
     PackageFilter,
     TourFilter,
 )
+from rest_framework.filters import OrderingFilter
 
 class CityListView(generics.ListAPIView):
     queryset = City.objects.all()
@@ -42,11 +48,52 @@ class AirportListView(generics.ListAPIView):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = AirportFilter
 
+# class PackageListView(generics.ListAPIView):
+#     queryset = Package.objects.all()
+#     serializer_class = PackageSerializer
+#     filter_backends = (DjangoFilterBackend,)
+#     filterset_class = PackageFilter
+    
 class PackageListView(generics.ListAPIView):
-    queryset = Package.objects.all()
     serializer_class = PackageSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = PackageFilter
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+
+    def get_queryset(self):
+        queryset = Package.objects.all().prefetch_related('tours')
+
+        # Get filter parameters from the request
+        tour_price_gte = self.request.query_params.get('tour_price__gte')
+        tour_price_lte = self.request.query_params.get('tour_price__lte')
+        tour_type = self.request.query_params.get('tour_type')  # Example filter for tour type
+
+        # Filter based on tour prices
+        if tour_price_gte or tour_price_lte:
+            tour_queryset = Tour.objects.filter(package=OuterRef('pk'))
+            if tour_price_gte:
+                queryset = queryset.annotate(min_price=Subquery(tour_queryset.values('price').filter(price__gte=tour_price_gte).order_by('price')[:1]))
+            if tour_price_lte:
+                queryset = queryset.annotate(max_price=Subquery(tour_queryset.values('price').filter(price__lte=tour_price_lte).order_by('-price')[:1]))
+
+            if tour_price_gte:
+                queryset = queryset.filter(min_price__isnull=False)
+            if tour_price_lte:
+                queryset = queryset.filter(max_price__isnull=False)
+
+        # Filter based on tour type if provided
+        if tour_type:
+            queryset = queryset.filter(tours__tour_type=tour_type).distinct()
+
+        return queryset
+
+    filterset_fields = {
+        'city': ['exact'],
+        'country': ['exact'],
+        'created_at': ['gte', 'lte'],
+        # Add more filters as needed
+    }
+
+    ordering_fields = ['created_at', 'tours__price', 'tours__start_date']  # Allow ordering by tour fields
+    ordering = ['created_at']  # Default ordering
 
 class TourListView(generics.ListAPIView):
     queryset = Tour.objects.all()
@@ -54,3 +101,48 @@ class TourListView(generics.ListAPIView):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TourFilter
     
+    
+class NavbarAPIView(APIView):
+    def get(self, request):
+        # Correctly prefetch related countries and cities
+        continents = Continent.objects.prefetch_related('countries__cities').all()
+        continent_data = ContinentSerializer(continents, many=True).data
+        
+        # Construct the nested structure for the navbar
+        navbar = []
+        for continent in continent_data:
+            continent_entry = {
+                "id": continent['id'],
+                "name": continent['name'],
+                "children": []
+            }
+            for country in continent.get('countries', []):
+                country_entry = {
+                    "id": country['id'],
+                    "name": country['name'],
+                    "children": []
+                }
+                for city in country.get('cities', []):
+                    city_entry = {
+                        "id": city['id'],
+                        "name": city['name'],
+                        "path": f"/{country['name']}-tour-{city['name']}"  # Customize path as needed
+                    }
+                    country_entry["children"].append(city_entry)
+
+                continent_entry["children"].append(country_entry)
+
+            navbar.append(continent_entry)
+        about = {
+            "id": "184",
+            "name": "درباره ما",
+            "path": "/about"
+        }
+        contact = {
+            "id": "780",
+            "name": "تماس با ما",
+            "path": "/contact"
+        }
+        navbar.append(about)
+        navbar.append(contact)
+        return Response(navbar)
