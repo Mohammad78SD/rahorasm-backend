12eeb13d5253a2e9d86bb5587829571ca6c730e4
Mohammad78SD <mohammadsd2000@gmail.com>
2024-09-30 17:06:30 +0330

    Refactor SignupSerializer in serializers.py

--- rahorasm/UserManager/serializers.py
+++ rahorasm/UserManager/serializers.py
@@ -17,6 +17,7 @@ class SignupSerializer(serializers.Serializer):
     password = serializers.CharField(max_length=128, write_only=True)
     name = serializers.CharField(max_length=100)
     
+    
 class ContactUsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactForm
