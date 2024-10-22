0360b1f3060c3774e7f5bd28235988d01a004608
Mohammad78SD <mohammadsd2000@gmail.com>
2024-10-22 17:37:13 +0330

    Refactor models.py to remove unused description fields in Continent, Country, City, and AirLine models

--- rahorasm/TourManager/models.py
+++ rahorasm/TourManager/models.py
@@ -3,7 +3,6 @@ from django_jalali.db import models as jmodels
 
 class Continent(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     edited_at = jmodels.jDateTimeField(auto_now=True)
     def __str__(self):
@@ -15,7 +14,6 @@ class Continent(models.Model):
 class Country(models.Model):
     name = models.CharField(max_length=200)
     continent = models.ForeignKey(Continent, on_delete=models.PROTECT, related_name='countries')
-    description = models.TextField(null=True, blank=True)
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     edited_at = jmodels.jDateTimeField(auto_now=True)
     def __str__(self):
@@ -27,7 +25,6 @@ class Country(models.Model):
 class City(models.Model):
     name = models.CharField(max_length=200)
     country = models.ForeignKey(Country, on_delete=models.PROTECT, related_name='cities')
-    description = models.TextField(null=True, blank=True)
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     edited_at = jmodels.jDateTimeField(auto_now=True)
     def __str__(self):
@@ -39,7 +36,6 @@ class City(models.Model):
 
 class AirLine(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     edited_at = jmodels.jDateTimeField(auto_now=True)
     def __str__(self):
@@ -60,35 +56,56 @@ class Airport(models.Model):
         verbose_name = "فرودگاه"
         verbose_name_plural = "فرودگاه ها"
     
-class Package(models.Model):
-    title = models.CharField(max_length=200)
-    city = models.ForeignKey(City, on_delete=models.PROTECT)
+    
+class Flight(models.Model):
+    tour = models.ForeignKey('Tour', on_delete=models.CASCADE, related_name='flights')
+    departure = jmodels.jDateTimeField()
+    arrival = jmodels.jDateTimeField()
+    origin_airport = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name='origin_airport')
+    destination_airport = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name='destination_airport')
+    return_departure = jmodels.jDateTimeField()
+    return_arrival = jmodels.jDateTimeField()
+    return_origin_airport = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name='return_origin_airport')
+    return_destination_airport = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name='return_destination_airport')
+    start_price = models.DecimalField(max_digits=20, decimal_places=2)
+    
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     edited_at = jmodels.jDateTimeField(auto_now=True)
-    def __str__(self):
-        return self.title
-    class Meta:
-        verbose_name = "پکیج"
-        verbose_name_plural = "پکیج ها"
     
+    
+    def __str__(self):
+        return f'{self.tour.title} تاریخ {self.departure}'
+    class Meta:
+        verbose_name = "رفت و برگشت"
+        verbose_name_plural = "رفت و برگشت ها"
+        
 class Tour(models.Model):
     title = models.CharField(max_length=200)
-    package = models.ForeignKey(Package, on_delete=models.PROTECT, related_name='tours')
     description = models.TextField()
+    destination = models.ForeignKey(City, on_delete=models.PROTECT)
     tour_type = models.CharField(max_length=200, choices=[('هوایی', 'هوایی'), ('زمینی', 'زمینی')])
-    is_featured = models.BooleanField(default=False)
-    tour_duration = models.CharField(max_length=200, default='7 روز')
     needed_documents = models.TextField()
     agency_service = models.TextField()
     tour_guide = models.TextField()
-    start_date = jmodels.jDateTimeField()
-    end_date = jmodels.jDateTimeField()
     airline = models.ForeignKey(AirLine, on_delete=models.PROTECT)
-    origin_airport = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name='origin_airport')
-    destination_airport = models.ForeignKey(Airport, on_delete=models.PROTECT, related_name='destination_airport')
-    price = models.DecimalField(max_digits=20, decimal_places=2)
+    tour_duration = models.CharField(max_length=200, default='7 روز')
+    is_featured = models.BooleanField(default=False)
+    least_price = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
+
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     edited_at = jmodels.jDateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        self.update_least_price()
+        
+    def update_least_price(self):
+        least_price = self.flights.aggregate(models.Min('start_price'))['start_price__min']
+        if least_price is not None:
+            self.least_price = least_price
+            super().save(update_fields=['least_price'])
+        
     def __str__(self):
         return self.title
     class Meta:
