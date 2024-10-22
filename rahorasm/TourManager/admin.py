47d2797084428689e59dee694e477c147ee0f68f
Mohammad78SD <mohammadsd2000@gmail.com>
2024-10-22 17:36:58 +0330

    Refactor admin.py to add Flight model and FlightAdmin for managing flights in tours

--- rahorasm/TourManager/admin.py
+++ rahorasm/TourManager/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-from .models import City, Country, AirLine, Airport, Package, Tour, Continent
+from .models import City, Country, AirLine, Airport, Tour, Continent, Flight
 
 admin.site.register(City)
 admin.site.register(Country)
 admin.site.register(Continent)
 admin.site.register(AirLine)
 admin.site.register(Airport)
-admin.site.register(Package)
-admin.site.register(Tour)
-# Register your models here.
+
+class FlightInline(admin.TabularInline):
+    model = Flight
+    extra = 1
+    
+@admin.register(Tour)
+class TourAdmin(admin.ModelAdmin):
+    list_display = ('title', 'tour_type', 'airline', 'is_featured', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('tour_type', 'is_featured', 'airline')
+    ordering = ('-created_at',)
+    inlines = [FlightInline]
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('tour', 'departure', 'arrival', 'origin_airport', 'destination_airport', 'start_price')
+    search_fields = ('tour__title', 'origin_airport__name', 'destination_airport__name')
+    list_filter = ('tour', 'origin_airport', 'destination_airport')
+    ordering = ('-departure',)
