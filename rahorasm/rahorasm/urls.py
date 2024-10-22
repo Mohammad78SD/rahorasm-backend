379ac41536d7b402bd94c9ca87266bd419971539
Mohammad78SD <mohammadsd2000@gmail.com>
2024-10-22 17:36:48 +0330

    Refactor settings.py to add REST framework JWT authentication and update visa path in navbar entries

--- rahorasm/rahorasm/urls.py
+++ rahorasm/rahorasm/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,7 +29,9 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('tour/', include('TourManager.urls')),
     path('visa/', include('VisaManager.urls')),
-    path('api/', include('misc.urls'))
+    path('api/', include('misc.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 
