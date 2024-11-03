eb83b48b874b9e886df7011a8d296e0cf69eab08
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-03 19:00:40 +0330

    Refactor TourManager serializers.py to add date conversion methods in FlightSerializer

--- rahorasm/TourManager/serializers.py
+++ rahorasm/TourManager/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import City, Country, AirLine, Airport, Tour, Continent, Flight
+import jdatetime
+import pytz
 
 
 class AirLineSerializer(serializers.ModelSerializer):
@@ -32,17 +34,42 @@ class AirportSerializer(serializers.ModelSerializer):
         model = Airport
         fields = '__all__'
 
+class TourInFlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tour
+        fields = '__all__'
+
 class FlightSerializer(serializers.ModelSerializer):
+    departure = serializers.SerializerMethodField()
+    # arrival = serializers.SerializerMethodField()
+    return_departure = serializers.SerializerMethodField()
+    return_arrival = serializers.SerializerMethodField()
+    
     origin_airport = AirportSerializer()
     destination_airport = AirportSerializer()
     return_origin_airport = AirportSerializer()
     return_destination_airport = AirportSerializer()
+    airline = AirLineSerializer()
+    tour = TourInFlightSerializer()
     class Meta:
         model = Flight
         fields = '__all__'
+        
+    def get_departure(self, obj):
+        jdate = obj.departure
+        print(jdate.tzinfo)
+        return jdate.togregorian()
+    def get_arrival(self, obj):
+        jdate = obj.arrival
+        return jdate.togregorian()
+    def get_return_departure(self, obj):
+        jdate = obj.return_departure
+        return jdate.togregorian()
+    def get_return_arrival(self, obj):
+        jdate = obj.return_arrival
+        return jdate.togregorian()
 
 class TourSerializer(serializers.ModelSerializer):
-    airline = AirLineSerializer()
     flights = FlightSerializer(many=True, read_only=True)
     class Meta:
         model = Tour
