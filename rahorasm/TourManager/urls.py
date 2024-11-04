34cd93cba3932b93df5639915d3128e13e6cd45f
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-04 17:50:06 +0330

    Refactor TourManager urls.py to add Filters view for tour filters

--- rahorasm/TourManager/urls.py
+++ rahorasm/TourManager/urls.py
@@ -8,6 +8,7 @@ from .views import (
     NavbarAPIView,
     TourDetailView,
     FlightDetailView,
+    Filters
 )
 
 urlpatterns = [
@@ -16,8 +17,10 @@ urlpatterns = [
     path('airlines/', AirLineListView.as_view(), name='airline_list'),
     path('airports/', AirportListView.as_view(), name='airport_list'),
     path('tours/', TourListView.as_view(), name='tour_list'),
+    path('filters/', Filters.as_view(), name='tour_filters'),
     path('tour/<int:pk>/', TourDetailView.as_view(), name='tour_detail'),
     path('flight/<int:pk>/', FlightDetailView.as_view(), name='flight_detail'),
     path('navbar/', NavbarAPIView.as_view(), name='navbar'),
+    
 
 ]
