e03ad8725b1c50a702610c6fbea460201b80de74
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-04 17:51:17 +0330

    Refactor VisaManager urls.py to update URL paths for visa views

--- rahorasm/VisaManager/urls.py
+++ rahorasm/VisaManager/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import VisaListCreateView, VisaDetailView
 
 urlpatterns = [
-    path('visas/', VisaListCreateView.as_view(), name='visa-list-create'),
-    path('visas/<int:pk>/', VisaDetailView.as_view(), name='visa-detail'),
+    path('list/', VisaListCreateView.as_view(), name='visa-list-create'),
+    path('<int:pk>/', VisaDetailView.as_view(), name='visa-detail'),
 ]
