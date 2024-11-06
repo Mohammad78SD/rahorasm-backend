85d3b69b0e65b6ca733a41458dd01962fc1030bb
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-06 18:40:32 +0330

    Refactor UserModel __str__ method to return phone number instead of name

--- rahorasm/UserManager/models.py
+++ rahorasm/UserManager/models.py
@@ -60,7 +60,7 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     def __str__(self):
-        return self.name
+        return self.phone_number
 
     
     class Meta:
