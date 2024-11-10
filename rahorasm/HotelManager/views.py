79384cabe88b40270789f79713070593a5d8c234
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-10 15:25:18 +0330

    Add API views for Hotel details and list with filtering options

--- rahorasm/HotelManager/views.py
+++ rahorasm/HotelManager/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
+from.models import Hotel
+from.serializers import HotelSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+
+class HotelDetails(generics.RetrieveAPIView):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+    
+class HotelList(generics.ListAPIView):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'city__name': ['exact'],
+        'city__country__name': ['exact'],
+        'city__country__continent__name': ['exact'],
+    }
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        city_name = self.request.query_params.get('city', None)
+        country_name = self.request.query_params.get('country', None)
+        continent_name = self.request.query_params.get('continent', None)
+        if city_name is not None:
+            queryset = queryset.filter(city__name=city_name)
+        if country_name is not None:
+            queryset = queryset.filter(city__country__name=country_name)
+        if continent_name is not None:
+            queryset = queryset.filter(city__country__continent__name=continent_name)
+        return queryset
