2ab8ef4f77d794974cfe1e875674e51f17176fe5
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-10 17:12:53 +0330

    Add HotelImage model and inline admin for image management

--- rahorasm/HotelManager/admin.py
+++ rahorasm/HotelManager/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
-from .models import Hotel, HotelPrice, HotelFacilities, RoomFacilities, RecreationalFacilities, SportFacilities
+from .models import Hotel, HotelPrice, HotelFacilities, RoomFacilities, RecreationalFacilities, SportFacilities, HotelImage
 
-# Register your models here.
-admin.site.register(Hotel)
+#i want to add inline field to add images to hotel in admin panel
+class HotelImageInline(admin.TabularInline):
+    model = HotelImage
+    extra = 1
+    fields = ('image','created_at')
+    readonly_fields = ('created_at', 'edited_at')
+    show_change_link = True
+    verbose_name = "تصویر"
+    verbose_name_plural = "تصاویر"
+
+class HotelAdmin(admin.ModelAdmin):
+    inlines = [HotelImageInline]
+
+admin.site.register(Hotel, HotelAdmin)
+
+
 admin.site.register(HotelPrice)
 admin.site.register(HotelFacilities)
 admin.site.register(RoomFacilities)
 admin.site.register(RecreationalFacilities)
 admin.site.register(SportFacilities)
+admin.site.register(HotelImage)
+class HotelImageAdmin(admin.ModelAdmin):
+    list_display = ('hotel', 'caption', 'uploaded_at')
+    list_filter = ('hotel', 'uploaded_at')
+    search_fields = ('caption', 'hotel__name')
