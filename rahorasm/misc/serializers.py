c4a6f15597cfb673c95e02e626608c60b764cc3a
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-11 16:07:05 +0330

    Add serializers for FooterBody, FooterColumn, and FooterContact models

--- rahorasm/misc/serializers.py
+++ rahorasm/misc/serializers.py
@@ -1,5 +1,26 @@
 from rest_framework import serializers
 from .models import ContactDetail, AboutDetail
+from rest_framework import serializers
+from .models import FooterBody, FooterColumn, FooterContact
+
+class FooterBodySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FooterBody
+        fields = ['title', 'link']
+
+class FooterColumnSerializer(serializers.ModelSerializer):
+    body = FooterBodySerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = FooterColumn
+        fields = ['title', 'body']
+
+class FooterContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FooterContact
+        fields = ['title', 'address', 'work_time', 'phone', 
+                 'email', 'instagram', 'telegram', 'whatsapp']
+        
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
