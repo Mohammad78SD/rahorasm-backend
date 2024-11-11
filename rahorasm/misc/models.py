edb4a3c2478d0bdb0f49d481417aca5b6f85e964
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-11 16:06:59 +0330

    Add FooterBody, FooterColumn, and FooterContact models with validation

--- rahorasm/misc/models.py
+++ rahorasm/misc/models.py
@@ -2,6 +2,40 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 
+class FooterBody(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
+    
+class FooterColumn(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.ManyToManyField(FooterBody)
+    def __str__(self):
+        return self.title
+    
+class FooterContact(models.Model):
+    title = models.CharField(max_length=100)
+    address = models.TextField()
+    work_time = models.CharField(max_length=200)
+    phone = models.TextField()
+    email = models.EmailField()
+    instagram = models.URLField()
+    telegram = models.URLField()
+    whatsapp = models.URLField()
+    
+    def clean(self):
+        if not self.pk and FooterContact.objects.exists():
+            raise ValidationError('Only one contact instance is allowed.')
+
+    def save(self, *args, **kwargs):
+        if not self.pk and FooterContact.objects.exists():
+            return FooterContact.objects.first()
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return "اطلاعات تماس"
+
 class ContactDetail(models.Model):
     ICON_CHOICES = [
         ('material-symbols-light:location-on-rounded', 'آیکن نقشه'),
