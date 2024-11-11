e1ef4af4ad481414f4451eaf2577fc570aee510b
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-11 16:07:09 +0330

    Add footer data endpoint to URLs

--- rahorasm/misc/urls.py
+++ rahorasm/misc/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ContactDetailList, AboutDetailList
+from .views import ContactDetailList, AboutDetailList, footer_data
 
 urlpatterns = [
     path('contactus/', ContactDetailList.as_view(), name='contactus'),
     path('aboutus/', AboutDetailList.as_view(), name='aboutus'),
+    path('footer/', footer_data, name='footer'),
 ]
