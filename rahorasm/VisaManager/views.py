7ac1077c41003973344da25ec9058258e9f6d3a6
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-11 16:08:17 +0330

    Add VisaListView to retrieve a list of visas using VisaListSerializer

--- rahorasm/VisaManager/views.py
+++ rahorasm/VisaManager/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Visa
-from .serializers import VisaSerializer
+from .serializers import VisaSerializer, VisaListSerializer
 
 class VisaListCreateView(generics.ListAPIView):
     queryset = Visa.objects.all()
@@ -9,3 +9,7 @@ class VisaListCreateView(generics.ListAPIView):
 class VisaDetailView(generics.RetrieveAPIView):
     queryset = Visa.objects.all()
     serializer_class = VisaSerializer
+    
+class VisaListView(generics.ListAPIView):
+    queryset = Visa.objects.all()
+    serializer_class = VisaListSerializer
