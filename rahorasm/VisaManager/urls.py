00482032d25445fc48068fad5c8bac69288847d7
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-11 16:07:44 +0330

    Add VisaListView endpoint for searching visas

--- rahorasm/VisaManager/urls.py
+++ rahorasm/VisaManager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import VisaListCreateView, VisaDetailView
+from .views import VisaListCreateView, VisaDetailView, VisaListView
 
 urlpatterns = [
     path('list/', VisaListCreateView.as_view(), name='visa-list-create'),
+    path('search/', VisaListView.as_view(), name='visa-list-search'),
     path('<int:pk>/', VisaDetailView.as_view(), name='visa-detail'),
 ]
