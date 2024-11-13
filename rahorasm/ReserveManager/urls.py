0e8a816c7303cb56eda345076cd9878841069a4e
Mohammad78SD <mohammadsd2000@gmail.com>
2024-11-13 17:56:26 +0330

    Add URL patterns for listing and retrieving reserves

--- rahorasm/ReserveManager/urls.py
+++ rahorasm/ReserveManager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateReserveView
+from .views import CreateReserveView, ListReserveView, RetrieveReserveView
 
 urlpatterns = [
     path('new/', CreateReserveView.as_view(), name='create-reserve'),
+    path('list/', ListReserveView.as_view(), name='list-reserve'),
+    path('<int:pk>/', RetrieveReserveView.as_view(), name='retrieve-reserve'),
 ]
