5057bd5311ffabd69e5358f72917863838c40372
Mohammad78SD <mohammadsd2000@gmail.com>
2024-12-16 19:14:40 +0330

    Add import for Jalali calendar widget in VisaManager admin.py

--- rahorasm/VisaManager/admin.py
+++ rahorasm/VisaManager/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Visa, Question
 
+# You need to import this for adding jalali calendar widget
+import django_jalali.admin as jadmin
+
+
 class QuestionInline(admin.TabularInline):
     model = Question
     extra = 1
