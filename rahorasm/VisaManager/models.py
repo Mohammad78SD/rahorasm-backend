284e6ec91da635fc9353327b2839fbd0ac60a4ec
Mohammad78SD <mohammadsd2000@gmail.com>
2024-12-16 19:15:52 +0330

    Enhance Visa and Question models with verbose names for improved localization

--- rahorasm/VisaManager/models.py
+++ rahorasm/VisaManager/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django_jalali.db import models as jmodels
 
 class Visa(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(max_length=200, verbose_name="نام کشور")
+    description = models.TextField(verbose_name="توضیحات")
     
     def __str__(self):
         return self.title
@@ -13,9 +13,9 @@ class Visa(models.Model):
         verbose_name_plural = "ویزاها"
     
 class Question(models.Model):
-    visa = models.ForeignKey(Visa, related_name='questions', on_delete=models.PROTECT)
-    question_text = models.CharField(max_length=255)
-    answer_text = models.TextField()
+    visa = models.ForeignKey(Visa, related_name='questions', on_delete=models.PROTECT, verbose_name="ویزا")
+    question_text = models.CharField(max_length=255, verbose_name="سوال")
+    answer_text = models.TextField(verbose_name="پاسخ")
     def __str__(self):
         return self.question_text
     class Meta:
