5813aca93b94b289aa4fb8fef1c8374733c80ce4
Mohammad78SD <mohammadsd2000@gmail.com>
2024-12-16 19:14:54 +0330

    Add nested admin integration and Jalali calendar support in TourManager admin

--- rahorasm/TourManager/admin.py
+++ rahorasm/TourManager/admin.py
@@ -1,5 +1,11 @@
+import nested_admin
 from django.contrib import admin
 from .models import City, Country, AirLine, Airport, Tour, Continent, Flight
+from HotelManager .models import HotelPrice
+from django_jalali.admin.filters import JDateFieldListFilter
+
+# You need to import this for adding jalali calendar widget
+import django_jalali.admin as jadmin
 
 admin.site.register(City)
 admin.site.register(Country)
@@ -7,18 +13,25 @@ admin.site.register(Continent)
 admin.site.register(AirLine)
 admin.site.register(Airport)
 
-class FlightInline(admin.TabularInline):
+
+class HotelPriceInline(nested_admin.NestedStackedInline):
+    model = HotelPrice
+    extra = 0
+
+class FlightInline(nested_admin.NestedStackedInline):
     model = Flight
     extra = 0
+    inlines = [HotelPriceInline]    
+    
     
 @admin.register(Tour)
-class TourAdmin(admin.ModelAdmin):
+class TourAdmin(nested_admin.NestedModelAdmin):
     list_display = ('title', 'tour_type', 'is_featured', 'created_at')
     search_fields = ('title', 'description')
     list_filter = ('tour_type', 'is_featured')
     ordering = ('-created_at',)
     inlines = [FlightInline]
-
+    
 @admin.register(Flight)
 class FlightAdmin(admin.ModelAdmin):
     list_display = ('tour', 'airline', 'departure', 'arrival', 'origin_airport', 'destination_airport', 'start_price')
