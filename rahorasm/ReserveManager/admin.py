5026c53c9e26ed02453f6c0a864b1c0d0653c87d
Mohammad78SD <mohammadsd2000@gmail.com>
2024-12-16 19:14:45 +0330

    Add import for Jalali calendar widget in ReserveManager admin.py

--- rahorasm/ReserveManager/admin.py
+++ rahorasm/ReserveManager/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Reserve, Person
 
+# You need to import this for adding jalali calendar widget
+import django_jalali.admin as jadmin
+
+
 admin.site.register(Reserve)
 admin.site.register(Person)
