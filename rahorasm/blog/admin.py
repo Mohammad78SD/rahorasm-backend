5899eecbe70f0144769bf84120f5cf77d2b313ea
Mohammad78SD <mohammadsd2000@gmail.com>
2024-12-16 19:13:44 +0330

    Add import for Jalali calendar widget in admin.py

--- rahorasm/blog/admin.py
+++ rahorasm/blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Post, Category, Comment
 
+
+# You need to import this for adding jalali calendar widget
+import django_jalali.admin as jadmin
+
+
+
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Comment)
