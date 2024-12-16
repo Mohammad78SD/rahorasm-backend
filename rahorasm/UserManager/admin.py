1762f187f6fbfa41e72c18c67ddf16ff4d32331c
Mohammad78SD <mohammadsd2000@gmail.com>
2024-12-16 19:15:32 +0330

    Add jalali calendar widget import to UserManager admin for enhanced date handling

--- rahorasm/UserManager/admin.py
+++ rahorasm/UserManager/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import UserModel, ContactForm
 
+# You need to import this for adding jalali calendar widget
+import django_jalali.admin as jadmin
+
+
 admin.site.register(UserModel)
 admin.site.register(ContactForm)
