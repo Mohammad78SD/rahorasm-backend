c9ea18160d8aac9a7c6db93453d7b0f0dbbd8c8b
Mohammad78SD <mohammadsd2000@gmail.com>
2025-01-19 23:13:36 +0330

    Enhance admin interface for Hotel and related models by adding search fields and autocomplete functionality

--- rahorasm/HotelManager/admin.py
+++ rahorasm/HotelManager/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Hotel, HotelPrice, HotelFacilities, RoomFacilities, RecreationalFacilities, SportFacilities, HotelImage
+from django import forms
 
 
 # You need to import this for adding jalali calendar widget
@@ -16,16 +17,77 @@ class HotelImageInline(admin.TabularInline):
     verbose_name_plural = "تصاویر"
 
 class HotelAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    autocomplete_fields = ['city', 'hotel_facilities', 'room_facilities', 'recreational_facilities', 'sport_facilities']
     inlines = [HotelImageInline]
 
 admin.site.register(Hotel, HotelAdmin)
 
 
-admin.site.register(HotelPrice)
-admin.site.register(HotelFacilities)
-admin.site.register(RoomFacilities)
-admin.site.register(RecreationalFacilities)
-admin.site.register(SportFacilities)
+
+class HotelPriceForm(forms.ModelForm):
+    class Meta:
+        model = HotelPrice
+        fields = '__all__'  # Include all fields in the form
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Format the initial values of price fields if they exist
+        for field in ['two_bed_price', 'one_bed_price', 'child_with_bed_price', 'child_no_bed_price']:
+            if self.instance and getattr(self.instance, field) is not None:
+                # Format the price for display
+                self.fields[field].initial = "{:,.2f}".format(getattr(self.instance, field))
+
+
+
+class HotelPriceAdmin(admin.ModelAdmin):
+    form = HotelPriceForm
+    autocomplete_fields = ['hotels']
+    search_fields = ['hotels__name']
+    # list_display = ('formatted_two_bed_price', 'formatted_one_bed_price', 
+    #                 'formatted_child_with_bed_price', 'formatted_child_no_bed_price')
+
+    # def formatted_two_bed_price(self, obj):
+    #     return "${:,.2f}".format(obj.two_bed_price)
+    # formatted_two_bed_price.short_description = "قیمت دو تخته"
+
+    # def formatted_one_bed_price(self, obj):
+    #     return "${:,.2f}".format(obj.one_bed_price)
+    # formatted_one_bed_price.short_description = "قیمت یک تخته"
+
+    # def formatted_child_with_bed_price(self, obj):
+    #     return "${:,.2f}".format(obj.child_with_bed_price)
+    # formatted_child_with_bed_price.short_description = "قیمت کودک با تخت"
+
+    # def formatted_child_no_bed_price(self, obj):
+    #     return "${:,.2f}".format(obj.child_no_bed_price)
+    # formatted_child_no_bed_price.short_description = "قیمت کودک بدون تخت"
+
+    # def save_model(self, request, obj, form, change):
+    #     # Convert formatted string back to Decimal before saving
+    #     for field in ['two_bed_price', 'one_bed_price', 'child_with_bed_price', 'child_no_bed_price']:
+    #         price_str = form.cleaned_data[field].replace(',', '')  # Remove commas
+    #         obj.__setattr__(field, price_str)  # Set the cleaned price value
+    #     super().save_model(request, obj, form, change)
+
+admin.site.register(HotelPrice, HotelPriceAdmin)
+
+
+
+
+
+class HotelFacilitiesAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+admin.site.register(HotelFacilities, HotelFacilitiesAdmin)
+class RoomFacilitiesAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+admin.site.register(RoomFacilities, RoomFacilitiesAdmin)
+class RecreationalFacilitiesAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+admin.site.register(RecreationalFacilities, RecreationalFacilitiesAdmin)
+class SportFacilitiesAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+admin.site.register(SportFacilities, SportFacilitiesAdmin)
 admin.site.register(HotelImage)
 class HotelImageAdmin(admin.ModelAdmin):
     list_display = ('hotel', 'caption', 'uploaded_at')
