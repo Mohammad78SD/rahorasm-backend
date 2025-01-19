380536aa2bc7c89f8efcda9edd8a4cb7dff9c140
Mohammad78SD <mohammadsd2000@gmail.com>
2025-01-19 23:13:00 +0330

    Enhance admin interface by adding search fields and autocomplete for Post and Category models

--- rahorasm/blog/admin.py
+++ rahorasm/blog/admin.py
@@ -4,9 +4,13 @@ from .models import Post, Category, Comment
 
 # You need to import this for adding jalali calendar widget
 import django_jalali.admin as jadmin
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+admin.site.register(Category, CategoryAdmin)
 
-
-
-admin.site.register(Post)
-admin.site.register(Category)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['author__name']
+    autocomplete_fields = ['author', 'category']
+    
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
