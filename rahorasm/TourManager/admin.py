64226cdd1086e92e48499dfb04df6ada45ded67c
Mohammad78SD <mohammadsd2000@gmail.com>
2025-01-19 23:14:38 +0330

    Refactor admin interface to enhance search and autocomplete features; add FlightTimes and FlightLeg models with inlines for better management

--- rahorasm/TourManager/admin.py
+++ rahorasm/TourManager/admin.py
@@ -1,40 +1,77 @@
 import nested_admin
 from django.contrib import admin
-from .models import City, Country, AirLine, Airport, Tour, Continent, Flight
+from .models import City, Country, AirLine, Airport, Tour, Continent, FlightLeg, FlightTimes
 from HotelManager .models import HotelPrice
 from django_jalali.admin.filters import JDateFieldListFilter
 
 # You need to import this for adding jalali calendar widget
 import django_jalali.admin as jadmin
 
-admin.site.register(City)
-admin.site.register(Country)
-admin.site.register(Continent)
-admin.site.register(AirLine)
-admin.site.register(Airport)
+
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    autocomplete_fields = ['country']
+admin.site.register(City, CityAdmin)
+
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    autocomplete_fields = ['continent']
+admin.site.register(Country, CountryAdmin)
+
+class ContinentAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+admin.site.register(Continent, ContinentAdmin)
+
+class AirLineAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    
+admin.site.register(AirLine, AirLineAdmin)
+
+class AirportAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    autocomplete_fields = ['city']
+admin.site.register(Airport, AirportAdmin)
+
+class HotelPriceInline(admin.TabularInline):
+    model = FlightTimes.hotel_price.through
+    extra = 1
+    autocomplete_fields = ['hotelprice']
+    verbose_name = "قیمت هتل"
+    verbose_name_plural = "قیمت هتل ها"
+class FlightLegsInline(admin.TabularInline):
+    model = FlightTimes.flight_Legs.through
+    extra = 1
+    autocomplete_fields = ['flightleg']
+    verbose_name = "پرواز"
+    verbose_name_plural = "پرواز ها"
+class FlightTimesAdmin(admin.ModelAdmin):
+    search_fields = ['departure_time']
+    autocomplete_fields = ['flight_Legs', 'hotel_price']
+    inlines = [FlightLegsInline, HotelPriceInline]
+admin.site.register(FlightTimes, FlightTimesAdmin)
 
 
-class HotelPriceInline(nested_admin.NestedStackedInline):
-    model = HotelPrice
-    extra = 0
-
-class FlightInline(nested_admin.NestedStackedInline):
-    model = Flight
-    extra = 0
-    inlines = [HotelPriceInline]    
+class FlightTimesInline(admin.TabularInline):  # or use StackedInline if you prefer a vertical layout
+    model = Tour.flight_times.through  # Use the through model for the Many-to-Many relationship
+    extra = 1  # Number of empty forms to show for adding new relations
+    verbose_name = "تاریخ پرواز"  # Custom name for the inline
+    verbose_name_plural = "تاریخ های پرواز"
     
     
 @admin.register(Tour)
-class TourAdmin(nested_admin.NestedModelAdmin):
+class TourAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['destinations']
     list_display = ('title', 'tour_type', 'is_featured', 'created_at')
-    search_fields = ('title', 'description')
+    search_fields = ('title', 'description_editor')
     list_filter = ('tour_type', 'is_featured')
-    ordering = ('-created_at',)
-    inlines = [FlightInline]
+    exclude = ('flight_times',)
+    inlines = [FlightTimesInline]
     
-@admin.register(Flight)
-class FlightAdmin(admin.ModelAdmin):
-    list_display = ('tour', 'airline', 'departure', 'arrival', 'origin_airport', 'destination_airport', 'start_price')
-    search_fields = ('tour__title', 'origin_airport__name', 'destination_airport__name')
-    list_filter = ('tour', 'origin_airport', 'destination_airport')
-    ordering = ('-departure',)
+    
+@admin.register(FlightLeg)
+class FlightLegAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['airline', 'departure_airport', 'arrival_airport']
+    list_display = ('airline', 'departure_airport', 'arrival_airport')
+    search_fields = ('departure_airport__name', 'arrival_airport__name')
+    list_filter = ('departure_airport', 'arrival_airport')
+    ordering = ('-departure_time',)
