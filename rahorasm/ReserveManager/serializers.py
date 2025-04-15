0aee996ca1173ea53255701687de9cf403c71af4
Mohammad78SD <mohammadsd2000@gmail.com>
2025-04-15 17:38:20 +0330

    Refactor serializers.py for improved readability and consistency in field formatting

--- rahorasm/ReserveManager/serializers.py
+++ rahorasm/ReserveManager/serializers.py
@@ -1,17 +1,35 @@
 from rest_framework import serializers
 from .models import Reserve, Person
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ['persian_name', 'english_name', 'national_code', 'passport_number', 'birth_date']
+        fields = [
+            "persian_name",
+            "english_name",
+            "national_code",
+            "passport_number",
+            "birth_date",
+        ]
+
 
 class ReserveSerializer(serializers.ModelSerializer):
-    hotel_name = serializers.CharField(source='hotel.name', read_only=True)
-    tour_name = serializers.CharField(source='tour.title', read_only=True)
+    hotel_name = serializers.CharField(source="hotel.name", read_only=True)
+    tour_name = serializers.CharField(source="tour.title", read_only=True)
+
     class Meta:
         model = Reserve
-        fields = ['tour_name', 'hotel_name','flight', 'two_bed_quantity', 'one_bed_quantity', 
-                  'child_with_bed_quantity', 'child_no_bed_quantity', 'final_price', 'status']
+        fields = [
+            "tour_name",
+            "hotel_name",
+            "two_bed_quantity",
+            "one_bed_quantity",
+            "child_with_bed_quantity",
+            "child_no_bed_quantity",
+            "final_price",
+            "status",
+        ]
